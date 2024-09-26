89206b9cc48385585cca740d4051023d992b16e7
Balmukund-01 <shuklabalmukund0@gmail.com>
2024-09-26 14:11:06 +0530

    removed e

--- math-notes/calc-fe/src/screens/home/index.tsx
+++ math-notes/calc-fe/src/screens/home/index.tsx
@@ -245,7 +245,7 @@ export default function Home() {
                 <Draggable 
                     key={index}
                     defaultPosition={latexPosition}
-                    onStop={(e, data) => setLatexPosition({x: data.x, y: data.y })}
+                    onStop={(data) => setLatexPosition({x: data.x, y: data.y })}
                     >
                         <div className="absolute text-white">
                             <div className="latex-content">{latex}</div>
