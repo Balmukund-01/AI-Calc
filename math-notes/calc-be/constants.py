cd70c52e037451ebe49a462cff5b6e8c49a54703
Balmukund-01 <shuklabalmukund0@gmail.com>
2024-09-27 16:34:51 +0530

    .env

--- math-notes/calc-be/constants.py
+++ math-notes/calc-be/constants.py
@@ -1,8 +1,26 @@
+# from dotenv import load_dotenv
+# import os
+# load_dotenv()
+
+# SERVER_URL = 'localhost'
+# PORT = '8900'
+# ENV = 'dev'
+# GEMINI_API_KEY=os.getenv('GEMINI_API_KEY')
+
 from dotenv import load_dotenv
 import os
+
+# Load environment variables from .env file
 load_dotenv()
 
-SERVER_URL = 'localhost'
-PORT = '8900'
-ENV = 'dev'
-GEMINI_API_KEY=os.getenv('GEMINI_API_KEY')
+# Change SERVER_URL based on the environment
+if os.getenv('ENV') == 'production':
+    SERVER_URL = '0.0.0.0'  # This allows the app to be accessible externally
+else:
+    SERVER_URL = 'localhost'
+
+# Use a default port
+PORT = os.getenv('PORT', '8900')  # Set default port to 8900 if not specified
+ENV = os.getenv('ENV', 'dev')  # Default to 'dev' if not specified
+GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
+
