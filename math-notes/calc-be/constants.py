0ab6ab8bc5073a86a9a032608ce589e7f6e56474
Balmukund-01 <shuklabalmukund0@gmail.com>
2024-09-27 18:54:36 +0530

    .env

--- math-notes/calc-be/constants.py
+++ math-notes/calc-be/constants.py
@@ -1,26 +1,27 @@
-# from dotenv import load_dotenv
-# import os
-# load_dotenv()
-
-# SERVER_URL = 'localhost'
-# PORT = '8900'
-# ENV = 'dev'
-# GEMINI_API_KEY=os.getenv('GEMINI_API_KEY')
-
 from dotenv import load_dotenv
 import os
-
-# Load environment variables from .env file
 load_dotenv()
 
-# Change SERVER_URL based on the environment
-if os.getenv('ENV') == 'production':
-    SERVER_URL = 'https://ai-calc-server.vercel.app'  # This allows the app to be accessible externally
-else:
-    SERVER_URL = 'localhost'
+# SERVER_URL = 'localhost'
+SERVER_URL = 'https://ai-calc-server.vercel.app'
+PORT = '8900'
+ENV = 'dev'
+GEMINI_API_KEY=os.getenv('GEMINI_API_KEY')
 
-# Use a default port
-PORT = os.getenv('PORT', '8900')  # Set default port to 8900 if not specified
-ENV = os.getenv('ENV', 'dev')  # Default to 'dev' if not specified
-GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
+# from dotenv import load_dotenv
+# import os
+
+# # Load environment variables from .env file
+# load_dotenv()
+
+# # Change SERVER_URL based on the environment
+# if os.getenv('ENV') == 'production':
+#     SERVER_URL = 'https://ai-calc-server.vercel.app'  # This allows the app to be accessible externally
+# else:
+#     SERVER_URL = 'localhost'
+
+# # Use a default port
+# PORT = os.getenv('PORT', '8900')  # Set default port to 8900 if not specified
+# ENV = os.getenv('ENV', 'dev')  # Default to 'dev' if not specified
+# GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
 
