2103414629bd13fb06501df9df7d942675595a7a
Balmukund-01 <shuklabalmukund0@gmail.com>
2024-10-01 14:12:36 +0530

    deployed

--- math-notes/calc-be/constants.py
+++ math-notes/calc-be/constants.py
@@ -1,8 +1,19 @@
+# from dotenv import load_dotenv
+# import os
+# load_dotenv()
+
+# SERVER_URL = 'localhost'
+# PORT = '8900'
+# ENV = 'dev'
+# GEMINI_API_KEY=os.getenv('GEMINI_API_KEY')
+
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
 
-SERVER_URL = 'localhost'
-PORT = '8900'
-ENV = 'dev'
-GEMINI_API_KEY=os.getenv('GEMINI_API_KEY')
+# Update with your production URL provided by the hosting platform
+SERVER_URL = os.getenv("SERVER_URL", "localhost")  # Replace with the actual production URL
+PORT = os.getenv("PORT", "8900")  # Default to 443 for HTTPS
+ENV = os.getenv("ENV", "dev")  # Set the environment to production
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
