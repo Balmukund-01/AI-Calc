ba42f60466972d94299b5848c4707447a57b3c16
Balmukund-01 <shuklabalmukund0@gmail.com>
2024-10-01 15:56:44 +0530

    local

--- math-notes/calc-be/constants.py
+++ math-notes/calc-be/constants.py
@@ -1,20 +1,8 @@
-# from dotenv import load_dotenv
-# import os
-# load_dotenv()
-
-# SERVER_URL = 'localhost'
-# PORT = '8900'
-# ENV = 'dev'
-# GEMINI_API_KEY=os.getenv('GEMINI_API_KEY')
-
-
-
-
 from dotenv import load_dotenv
 import os
 load_dotenv()
 
-SERVER_URL = os.getenv("SERVER_URL")
-PORT = os.getenv('PORT')
-ENV = ('ENV')
+SERVER_URL = 'localhost'
+PORT = '8900'
+ENV = 'dev'
 GEMINI_API_KEY=os.getenv('GEMINI_API_KEY')
